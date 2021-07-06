0783dfb6f20bc0e84871942c29975d70440e3427
Rishi <64310471+Rishi-Bidani@users.noreply.github.com>
2021-07-06 14:41:57 +0530

    Initial commit

---
+++ server/main.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const path = require("path");
+const cors = require("cors");
+
+// MiddleWare
+app.use(express.json());
+app.use(cors());
+
+// Any posts requests will be handled by the following file
+const posts = require("./routes/posts");
+app.use("/posts", posts);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/.*/", (req, res) => {
+    // res.send("Hello World");
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+const port = process.env.PORT || 5000;
+http.listen(port, () => {
+    console.log("Website live on: " + `http://localhost:${port}`);
+});
