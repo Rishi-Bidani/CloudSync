775921453b5078e34f178ad8de7ef164473d99e4
sandflavoured <saminhaque.sh@gmail.com>
2021-09-28 01:27:01 +1000

    Added new assets and vue components
    Components for the top nav bar and the features section of the main landing page.

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <!--
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  -->
+  <div>
+    <TopBar msg=""/>
+  <Features msg=""/>
+  </div>
+  
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import TopBar from './components/TopBar.vue'
+import Features from './components/Features.vue'
+
+export default {
+  name: 'App',
+  components: {
+    //HelloWorld,
+    TopBar,
+    Features
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
