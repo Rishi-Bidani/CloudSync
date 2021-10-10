f20132e04f459020f0dada3638c7f9f4c0200ddb
Rishi <64310471+Rishi-Bidani@users.noreply.github.com>
2021-10-10 12:23:30 +0530

    fixed bug: Displayed file sizes were absurd

--- HOST WEBSITE/server/js/fsWrap.js
+++ HOST WEBSITE/server/js/fsWrap.js
@@ -22,12 +22,22 @@ class fsWrapper {
         const read = await fsn.readdir(fixedpath, {
             withFileTypes: true,
         });
+        const MB = (byteSize) => {
+            return (byteSize / (1024 * 1024)).toFixed(3)
+        }
+        const KB = (byteSize) => {
+            return (byteSize / 1024).toFixed(3)
+        }
         for (const file of read) {
             if (file.isFile()) {
+                const fileSizeBytes =
+                    Number.parseFloat(
+                        fs.statSync(path.join(fixedpath, file.name)).size
+                    ).toFixed(3)
                 files.push({
                     fileName: file.name,
                     fileExtension: path.extname(file.name),
-                    fileSize: Number.parseFloat(fs.statSync(path.join(fixedpath, file.name)).size / 1024).toFixed(3),
+                    fileSize: KB(fileSizeBytes) >= 1 ? MB(fileSizeBytes) : KB(fileSizeBytes),
                 });
             } else if (file.isDirectory()) {
                 folders.push(file.name);
